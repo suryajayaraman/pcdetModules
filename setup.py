a3992e42a50de527893528a05f25e677ea0a84c1
suryajayaraman <surya.jayaraman@cat.com>
2023-02-27 12:04:08 +0530

    testing iou3d nms

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-# import os
-
+import os
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
-# def make_cuda_ext(name, module, sources):
-#     cuda_ext = CUDAExtension(
-#         name='%s.%s' % (module, name),
-#         sources=[os.path.join(*module.split('.'), src) for src in sources]
-#     )
-#     return cuda_ext
+def make_cuda_ext(name, module, sources):
+    cuda_ext = CUDAExtension(
+        name='%s.%s' % (module, name),
+        sources=[os.path.join(*module.split('.'), src) for src in sources]
+    )
+    return cuda_ext
 
 
 
@@ -17,13 +16,16 @@ if __name__ == '__main__':
     setup(
         name='pcdetModules',
         ext_modules=[
-        CUDAExtension('iou3d_nms_cuda', 
-            [
-                'src/iou3d_cpu.cpp',
-                'src/iou3d_nms_api.cpp',
-                'src/iou3d_nms.cpp',
-                'src/iou3d_nms_kernel.cu',
-            ])
+           make_cuda_ext(
+                name='iou3d_nms_cuda',
+                module='iou3d_nms',
+                sources=[
+                    'src/iou3d_cpu.cpp',
+                    'src/iou3d_nms_api.cpp',
+                    'src/iou3d_nms.cpp',
+                    'src/iou3d_nms_kernel.cu',
+                ]
+            )
         ],
         cmdclass={'build_ext': BuildExtension}
     )
